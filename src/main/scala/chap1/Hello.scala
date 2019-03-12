349df7667551cc74ed83244f8fc3f6cb80ba6736
ZehuaWang <transforminottawa@gmail.com>
2019-03-11 23:19:55 -0400

    添加制表符的说明

--- src/main/scala/chap1/Hello.scala
+++ src/main/scala/chap1/Hello.scala
@@ -12,6 +12,18 @@ object Hello {
 
   def main(args: Array[String]): Unit = {
     println("hello,scala")
+
+    //定义变量
+    var n1 : Int = 1
+    var N1 : Int = 2
+
+    //Scala最好是一行一条语句 最后不要带分号
+
+    println("namea\tge") //制表符
+    println("k\"kk")
+    println("你好 \nhello")
+    println("abc\ru")
+
   }
 
 }
